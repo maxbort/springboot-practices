9b559865abae0c8c2d0dd9d3ca1836268463b49e
CHOI HyoJun <cjfgh2449@gmail.com>
2024-06-24 16:54:28 +0900

    feat. @SpringBootConfiguration

--- springboot-helloworld/src/main/java/com/poscodx/springboot/helloworld/HelloWorldApplication.java
+++ springboot-helloworld/src/main/java/com/poscodx/springboot/helloworld/HelloWorldApplication.java
@@ -22,8 +22,15 @@ public class HelloWorldApplication {
 	public static void main(String[] args) {
 		
 		/**
-		 * 	SpringApplication.run(...) 안에서 일어나는 일
-		 * 
+		 * 	SpringApplication.run(...) 안에서 일어나는 일 : BootStrap
+		 * 1. 어플리케이션 컨테스트(Application Context, Spring Container) 생성
+		 * 2. 만약, 웹 어플리케이션이면 웹어플리케이션 타입 결정(SpringMVC, Reactive)
+		 * 3. 어노테이션 스캐닝 + Configuration Class를 통해서 빈 생성/등록/와이어링 
+		 * 4. 만약 웹 어플리케이션이고 타입이 Spring MVC 이면 
+		 * 				- 내장(embeded) 서버 (TomcatEmbededServiceServletContatiner 인스턴스 생성)
+		 * 				- 서버 인스턴스에 웹 어플리케이션을 배포
+		 * 				- 서버 인스턴스 실행
+		 * 5. ApplicationRunner 인터페이스를 구현한 빈을 Application Context에서 찾아서 실행
 		 */
 		
 //		ApplicationContext ac = null;
